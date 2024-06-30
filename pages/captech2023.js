e40b89f0c50843909ca91744d04e6565714a72da
Artilleryxd <karandwari@gmail.com>
2024-07-01 01:16:40 +0530

    removed runtime error

---
+++ pages/captech2023.js
@@ -0,0 +1,15 @@
+import { Captech2023 } from "@/sections";
+import Head from "next/head";
+
+const CapTech2023 = () => {
+  return (
+    <>
+      <Head>
+        <title>Patrons</title>
+      </Head>
+      <Captech2023 />
+    </>
+  );
+};
+
+export default CapTech2023;
