17cc9a972f8e38cb6aef0e68e2f60780250e8837
Akshad Gawde <gawdeakshad@gmail.com>
2024-07-01 01:22:17 +0530

    deploy check

--- sections/index.js
+++ sections/index.js
@@ -7,8 +7,21 @@ import Contact from "./Contact";
 import Advisory_board from "./Advisory_board";
 import Executive_board from "./Executive_board";
 import Committees from "./Committees";
-import Patrons from "./Patrons"
-import Speakers from  "./Speakers"
+import Patrons from "./Patrons";
+import Speakers from "./Speakers";
 import Captech2023 from "./Captech2023";
 
-export { Hero, Agency, Team, ShowCase, Blog, Contact, Advisory_board, Executive_board, Committees, Patrons, Speakers, Captech2023 };
+export {
+  Hero,
+  Agency,
+  Team,
+  ShowCase,
+  Blog,
+  Contact,
+  Advisory_board,
+  Executive_board,
+  Committees,
+  Patrons,
+  Speakers,
+  Captech2023,
+};
