02ebc88aa59b21e54df9948db85eb7d17633dca3
Karan N Dwari <kron@Karans-MacBook-Air.local>
2024-07-01 12:12:26 +0530

    Added marquee

---
+++ components/common/Marqueee.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Marquee from 'react-fast-marquee'
+
+
+function Marqueee() {
+  return (
+    <div>
+    <Marquee pauseOnHover="true">
+    I can be a React component, multiple React components, or just some text.
+        </Marquee>  
+    </div>
+  );
+}
+
+
+export default Marqueee;
