70bc265e3c172649a61c3d5fb9d0dc8704f31d42
Akshad Gawde <gawdeakshad@gmail.com>
2024-07-03 13:00:25 +0530

    fixed marquee

--- components/common/Marqueee.js
+++ components/common/Marqueee.js
@@ -6,7 +6,7 @@ function Marqueee() {
     <div>
       <a href="https://sipbn.com.au/captech2024/">
         <Marquee pauseOnHover="true" style={{ fontSize: 24, padding: 10 }}>
-          #CapTech2024- "Discover. Connect. Execute"- Monday 25th November 2024
+          #CapTech2024- "Discover. Connect. Execute" | Monday 25th November 2024
           to Thursday 28th November 2024 | ICC Sydney Australia
         </Marquee>
       </a>
