1c1c59337e17fa3742191a550a75e1be7544a68d
Shivansh <shivansh.kukreti.2004@gmail.com>
2024-07-05 10:55:28 +0530

    pkage

--- sections/Team.js
+++ sections/Team.js
@@ -14,10 +14,7 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineContent from '@mui/lab/TimelineContent';
 import TimelineOppositeContent from '@mui/lab/TimelineOppositeContent';
 import TimelineDot from '@mui/lab/TimelineDot';
-import FastfoodIcon from '@mui/icons-material/Fastfood';
-import LaptopMacIcon from '@mui/icons-material/LaptopMac';
-import HotelIcon from '@mui/icons-material/Hotel';
-import RepeatIcon from '@mui/icons-material/Repeat';
+
 import Typography from '@mui/material/Typography';
 
 const Team = () => {
