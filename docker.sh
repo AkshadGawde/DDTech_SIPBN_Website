15e1c4fe576941a6fa22eeed452d6d6dc08dbcaf
PrabirKalwani <prabir.kalwani@gmail.com>
2024-07-06 16:54:14 +0530

    New : Added Dockerfile and Bash Script

---
+++ docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Build the Docker image with the tag "akshad_website"
+docker build -t akshad_website .
+
+# Run a Docker container from the "akshad_website" image and map port 3000
+docker run -p 3000:3000 akshad_website -d 
