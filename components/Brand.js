b65c0e4eaa8a14536db354b6620e81ee46905fd3
Akshad Gawde <gawdeakshad@gmail.com>
2024-07-12 15:58:35 +0530

    ast1

--- components/Brand.js
+++ components/Brand.js
@@ -1,37 +1,47 @@
-import React from "react"
-import { Title } from "./common/Title"
-import { brand } from "@/assets/data/dummydata"
-import { brand2 } from "@/assets/data/dummydata"
+import React from "react";
+import { Title } from "./common/Title";
+import { brand } from "@/assets/data/dummydata";
+import { brand2 } from "@/assets/data/dummydata";
 
 const Brand = () => {
   return (
     <>
-      <section className='brand'>
-        <div className='container'>
-          <div className='heading-title'>
-            <Title title='Our Partners' />
+      <section className="brand">
+        <div className="container">
+          <div className="heading-title">
+            <Title title="Our Partners" />
           </div>
-          <div className='brand-content grid-8 py'>
+          <div className="brand-content grid-8 py">
             {brand.map((item) => (
-              <div className='images' key={item.id}>
-                <img src={item.cover} alt={item.id} width='100%' height='100%'  />
+              <div className="images" key={item.id}>
+                <img
+                  src={item.cover}
+                  alt={item.id}
+                  width="100%"
+                  height="100%"
+                />
               </div>
             ))}
           </div>
-          <div className='heading-title'>
-            <Title title='In the Media' />
+          <div className="heading-title">
+            <Title title="In the Media" />
           </div>
-          <div className='brand-content2 grid-7 py'>
+          <div className="brand-content grid-7 py">
             {brand2.map((item) => (
-              <div className='images' key={item.id}>
-                <img src={item.cover} alt={item.id} width='100%' height='100%' />
+              <div className="images" key={item.id}>
+                <img
+                  src={item.cover}
+                  alt={item.id}
+                  width="100%"
+                  height="100%"
+                />
               </div>
             ))}
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Brand
+export default Brand;
