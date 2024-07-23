f7775615c2ed314cca03ef6a71a202c1b37ed802
Akshad Gawde <gawdeakshad@gmail.com>
2024-07-23 14:14:24 +0530

    rearranged partners

--- sections/Hero.js
+++ sections/Hero.js
@@ -22,7 +22,10 @@ const Hero = () => {
           </a>
           <h1 className="hero-title">DISCOVER . CONNECT . EXECUTE</h1>
           <TitleSm title="Venue: ICC Sydney 14 Darling Drive Sydney, NSW 2000 Australia" />
-          <p>Tue, 26 Nov 2024 9:00 AM - Wed, 27 Nov 2024 9:00 PM AEDT</p>
+          <p>
+            Main event - Tuesday 26 November 2024 <br />
+            8:00am - 7:30pm
+          </p>
           <a
             href="https://www.eventbrite.com.au/e/captech2024-discover-connect-execute-venue-icc-sydney-tickets-772015889307"
             target="_blank"
