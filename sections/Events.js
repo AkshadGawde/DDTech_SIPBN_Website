44aad0d83b56196f66edf43823a2367c4de5b56d
Artilleryxd <karandwari@gmail.com>
2024-07-30 22:44:37 +0530

    fixed images

--- sections/Events.js
+++ sections/Events.js
@@ -71,42 +71,46 @@ const Events = () => {
                   Economic Update - Opportunities and Challenges
                 </p>
               </div>
-              <a href="https://www.eventbrite.com.au/e/#CapTech2024-global-pitching-presenting-session-tickets-765412678927">
-                <img
-                  className="eventImg"
-                  src="https://res.cloudinary.com/duv9rw1wg/image/upload/v1719355161/homePage/eqqseipvvxs11lsk1fj0.png"
-                  alt="Event 1 Image"
-                />
-              </a>
-              <h2 class="event-title">February 2024</h2>
-              <p class="event-description">Pitching Session #CapTech2024</p>
+              <div className="eventCard">
+                <a href="https://www.eventbrite.com.au/e/#CapTech2024-global-pitching-presenting-session-tickets-765412678927">
+                  <img
+                    className="eventImg"
+                    src="https://res.cloudinary.com/duv9rw1wg/image/upload/v1719355161/homePage/eqqseipvvxs11lsk1fj0.png"
+                    alt="Event 1 Image"
+                  />
+                </a>
+                <h2 class="event-title">February 2024</h2>
+                <p class="event-description">Pitching Session #CapTech2024</p>
+              </div>
             </div>
             <div class="eventCard">
-              <a href="https://www.eventbrite.com.au/e/global-pitching-session-preliminary-round-#CapTech2023-tickets-694439807157">
-                <img
-                  className="eventImg"
-                  src="https://res.cloudinary.com/duv9rw1wg/image/upload/v1719355161/homePage/rdvah2y9vbzr1zyjft5s.png"
-                  alt="Event 2 Image"
-                />
-              </a>
-              <h2 class="event-title">September 2023</h2>
-              <p class="event-description">
-                {" "}
-                Global Pitching Session – Preliminary Round #CapTech2023
-              </p>
-            </div>
-            <div class="eventCard">
-              <a href="https://www.eventbrite.com.au/e/#CapTech2023-festival-where-business-growth-meets-capital-24-27-oct-23-tickets-482131798187">
-                <img
-                  className="eventImg"
-                  src="https://res.cloudinary.com/duv9rw1wg/image/upload/v1719355161/homePage/qwjduvdedvvujnmiuken.png"
-                  alt="Event 3 Image"
-                />
-              </a>
-              <h2 class="event-title">October 2023</h2>
-              <p class="event-description">
-                #CapTech2023 Welcome Drinks at NSW Parliament House
-              </p>
+              <div className="eventCard">
+                <a href="https://www.eventbrite.com.au/e/global-pitching-session-preliminary-round-#CapTech2023-tickets-694439807157">
+                  <img
+                    className="eventImg"
+                    src="https://res.cloudinary.com/duv9rw1wg/image/upload/v1719355161/homePage/rdvah2y9vbzr1zyjft5s.png"
+                    alt="Event 2 Image"
+                  />
+                </a>
+                <h2 class="event-title">September 2023</h2>
+                <p class="event-description">
+                  {" "}
+                  Global Pitching Session – Preliminary Round #CapTech2023
+                </p>
+              </div>
+              <div class="eventCard">
+                <a href="https://www.eventbrite.com.au/e/#CapTech2023-festival-where-business-growth-meets-capital-24-27-oct-23-tickets-482131798187">
+                  <img
+                    className="eventImg"
+                    src="https://res.cloudinary.com/duv9rw1wg/image/upload/v1719355161/homePage/qwjduvdedvvujnmiuken.png"
+                    alt="Event 3 Image"
+                  />
+                </a>
+                <h2 class="event-title">October 2023</h2>
+                <p class="event-description">
+                  #CapTech2023 Welcome Drinks at NSW Parliament House
+                </p>
+              </div>
             </div>
             <div class="eventCard">
               <a href="https://www.eventbrite.com.au/e/#CapTech2023-festival-where-business-growth-meets-capital-24-27-oct-23-tickets-482131798187">
