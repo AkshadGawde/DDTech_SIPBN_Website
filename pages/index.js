d1c0094fcbb7aaf9f6390b44d719809b8d706a72
Shivansh <shivansh.kukreti.2004@gmail.com>
2024-08-18 21:24:25 +0530

    SEO_metadata change

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,24 @@
 import { Hero } from "@/sections";
 import Head from "next/head";
-import Marqueee from "@/components/common/Marqueee";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>SIPBN</title>
+        <meta name="description" content="Sydney Investors, Professionals and Business Networking Group Incorporated (SIPBN)" />
+        <meta name="keywords" content="Innovation, Summit, Technology, Leadership, Collaboration, Networking, Future-focused, Funding, Equity, Business match making, Policy, Thought leadership, Venture, Debt Funding, Transformation, Private Equity, Empowerment, Pitching sessions, Advancement, Private Capital, dealmaking" />
+        <meta property="og:title" content="SIPBN" />
+        <meta property="og:description" content="Sydney Investors, Professionals and Business Networking Group Incorporated (SIPBN)" />
+        <meta property="og:url" content="https://sipbn.com.au/" />
+        <meta property="og:image" content="" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="SIPBN" />
+        <meta name="twitter:description" content="Sydney Investors, Professionals and Business Networking Group Incorporated (SIPBN)" />
+        <meta name="twitter:image" content="" />
+       
       </Head>
       
-    
       <Hero />
     </>
   );
