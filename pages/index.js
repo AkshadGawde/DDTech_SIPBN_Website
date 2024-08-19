43f6f98f331a3689a5e7edbf61cb43012f78289d
Shivansh <shivansh.kukreti.2004@gmail.com>
2024-08-19 12:30:53 +0530

    seo2

--- pages/index.js
+++ pages/index.js
@@ -8,10 +8,14 @@ export default function Home() {
         <title>SIPBN</title>
         <meta name="description" content="Sydney Investors, Professionals and Business Networking Group Incorporated (SIPBN)" />
         <meta name="keywords" content="Innovation, Summit, Technology, Leadership, Collaboration, Networking, Future-focused, Funding, Equity, Business match making, Policy, Thought leadership, Venture, Debt Funding, Transformation, Private Equity, Empowerment, Pitching sessions, Advancement, Private Capital, dealmaking" />
+        
+        {/* Open Graph Meta Tags */}
         <meta property="og:title" content="SIPBN" />
         <meta property="og:description" content="Sydney Investors, Professionals and Business Networking Group Incorporated (SIPBN)" />
         <meta property="og:url" content="https://sipbn.com.au/" />
         <meta property="og:image" content="https://lh3.googleusercontent.com/p/AF1QipMdzWweOhF8941oxkqrc6p0SBT2Sp4XOK-SlXwY=s1360-w1360-h1020" />
+
+        {/* Twitter Card Meta Tags */}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="SIPBN" />
         <meta name="twitter:description" content="Sydney Investors, Professionals and Business Networking Group Incorporated (SIPBN)" />
