18828ae70fcd697bd6a2251ec7836d170e02edbd
Artilleryxd <karandwari@gmail.com>
2024-09-13 21:07:23 +0530

    Added pictures and svg

--- components/HeroPage/BenefitsAcitivity.js
+++ components/HeroPage/BenefitsAcitivity.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import benefit1 from "@/public/images/benefit1.png"
 
 const BenefitsActivities = () => {
   const [activeTab, setActiveTab] = useState("benefits");
@@ -27,22 +28,22 @@ const BenefitsActivities = () => {
       {activeTab === "benefits" && (
         <div className="benefits-content">
           <div className="benefit-item">
-            <img src="" alt="Elevate Your Brand" />
+            <img src="https://img.icons8.com/?size=100&id=999&format=png&color=FFFFFF" alt="Elevate Your Brand" />
             <h3>Elevate Your Brand</h3>
             <p>Skyrocket positioning, awareness, and visibility</p>
           </div>
           <div className="benefit-item">
-            <img src="" alt="Unlock New Revenue" />
+            <img src="https://img.icons8.com/?size=100&id=63371&format=png&color=FFFFFF" alt="Unlock New Revenue" />
             <h3>Unlock New Revenue</h3>
             <p>Unparalleled business acquisition and prospecting</p>
           </div>
           <div className="benefit-item">
-            <img src="" alt="Be the Authority" />
+            <img src="https://img.icons8.com/?size=100&id=boPtdpDS9FXC&format=png&color=FFFFFF" alt="Be the Authority" />
             <h3>Be the Authority</h3>
             <p>Position as an undisputed thought leader</p>
           </div>
           <div className="benefit-item">
-            <img src="" alt="Elite Connections" />
+            <img src="https://img.icons8.com/?size=100&id=1846&format=png&color=FFFFFF" alt="Elite Connections" />
             <h3>Elite Connections</h3>
             <p>
               Access an exclusive network of 2,500+ senior executives from
@@ -55,24 +56,24 @@ const BenefitsActivities = () => {
       {activeTab === "activities" && (
         <div className="activities-content">
           <div className="activity-item">
-            <img src="/path-to-image5.png" alt="Prime Brand Visibility" />
+            <img src="https://img.icons8.com/?size=100&id=38511&format=png&color=FFFFFF" alt="Prime Brand Visibility" />
             <h3>Prime Brand Visibility</h3>
             <p>Your brand in front of thousands of decision makers</p>
           </div>
           <div className="activity-item">
-            <img src="/path-to-image6.png" alt="Private Events" />
+            <img src="https://img.icons8.com/?size=100&id=1074&format=png&color=FFFFFF" alt="Private Events" />
             <h3>Private Events</h3>
             <p>
               Host exclusive gatherings of selected guests and keynote speakers
             </p>
           </div>
           <div className="activity-item">
-            <img src="/path-to-image7.png" alt="Insider Access" />
+            <img src="https://img.icons8.com/?size=100&id=152&format=png&color=FFFFFF" alt="Insider Access" />
             <h3>Insider Access</h3>
             <p>In-company sessions led by World Business Forum speakers</p>
           </div>
           <div className="activity-item">
-            <img src="/path-to-image8.png" alt="Networking Hotpots" />
+            <img src="https://img.icons8.com/?size=100&id=62135&format=png&color=FFFFFF" alt="Networking Hotpots" />
             <h3>Networking Hotpots</h3>
             <p>Unique activations in high-traffic areas</p>
           </div>
--- components/HeroPage/BusinessForum.js
+++ components/HeroPage/BusinessForum.js
@@ -5,7 +5,9 @@ const BusinessForum = () => {
     <div className="business-forum">
       <div className="business-forum__content">
         <div className="business-forum__images">
-          <div className="business-forum__image image-1"></div>
+          <div className="business-forum__image image-1">
+            <img src="" alt="" className="squareImage"/>
+          </div>
           <div className="business-forum__image image-2"></div>
           <div className="business-forum__image image-3"></div>
           <div className="business-forum__image image-4"></div>
