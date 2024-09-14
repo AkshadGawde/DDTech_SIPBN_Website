b303f20c2e10158acd838e08049f5cb5bc0163c4
Artilleryxd <karandwari@gmail.com>
2024-09-14 14:37:23 +0530

    Minor tweaks

--- components/HeroPage/SponsCarousel.js
+++ components/HeroPage/SponsCarousel.js
@@ -1,117 +1,37 @@
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import React, { useState } from "react";
-import Slider from "react-slick";
-import Image from "next/image";
+import React from 'react';
 
-const SponsCarousel = () => {
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const sliderSettings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
+// Dummy images for demonstration
+const images = [
+  { src: "/images/c1.jpg", alt: "Sponsor 1" },
+  { src: "/images/c2.jpg", alt: "Sponsor 2" },
+  { src: "/images/c3.jpg", alt: "Sponsor 3" },
+  { src: "/images/c4.jpg", alt: "Sponsor 4" },
+];
 
-    arrows: false,
-    beforeChange: (oldIndex, newIndex) => {
-      setCurrentSlide(newIndex); // Update current slide index
-    },
-    customPaging: (i) => (
-      <div
-        className="custom-dot"
-        style={{
-          opacity: i === currentSlide ? 1 : 0.5, // Change opacity based on whether it's the current slide
-        }}
-      ></div>
-    ),
-  };
-
-  const brandimages = [
-    {
-      src: "../SVG/Logo1.svg",
-      width: 600,
-      height: 900,
-      alt: "carousel Image 3",
-    },
-    {
-      src: "../SVG/Logo2.svg",
-      width: 600,
-      height: 900,
-      alt: "carousel Image 6",
-    },
-    {
-      src: "../SVG/Logo3.svg",
-      width: 600,
-      height: 900,
-      alt: "carousel Image 2",
-    },
-    {
-      src: "../SVG/Logo4.svg",
-      width: 600,
-      height: 900,
-      alt: "carousel Image 0",
-    },
-    {
-      src: "../SVG/Logo5.svg",
-      width: 600,
-      height: 900,
-      alt: "carousel Image 5",
-    },
-    {
-      src: "../SVG/Logo6.svg",
-      width: 600,
-      height: 900,
-      alt: "carousel Image 7",
-    },
-    {
-      src: "../SVG/Logo7.svg",
-      width: 600,
-      height: 900,
-      alt: "carousel Image 8",
-    },
-  ];
-
+function SponsCarousel() {
   return (
-    <div className="brand-carousel">
-      {/* <h1>Our Partners</h1> */}
-      <div className="carouselHolder">
-        <Slider {...sliderSettings} className="slider">
-          {brandimages.map((image, index) => (
-            <div key={index} className="slide">
-              <Image {...image} className="slide-image" />
-            </div>
+    <div className='sliderContainer'>
+      <div className="sliderWrapper">
+        <div className="sliderCarousel">
+          {images.map((image, index) => (
+            <img 
+              key={index} 
+              src={image.src} 
+              alt={image.alt} 
+              id={`slider${index + 1}`} 
+              className="sliderImage"
+            />
           ))}
-        </Slider>
-      </div>
-      <div className="outerHrHolder">
-        <div className="innerHr1"></div>
-        <p>Our Partners</p>
-        <div className="innerHr2"></div>
+        </div>
+        <div className="sliderNav">
+          <a href="#slider1"></a>
+          <a href="#slider2"></a>
+          <a href="#slider3"></a>
+          <a href="#slider4"></a>
+        </div>
       </div>
     </div>
   );
-};
+}
 
 export default SponsCarousel;
