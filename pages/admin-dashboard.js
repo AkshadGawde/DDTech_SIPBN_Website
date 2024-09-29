12c4005a81ab3ee447aced3f6d753648c48d5d51
Bhavya Chopra <64201249+bhavyachopra99@users.noreply.github.com>
2024-09-29 11:44:17 +0530

    admin fixed + ticketing basic
    gg

--- pages/admin-dashboard.js
+++ pages/admin-dashboard.js
@@ -4,13 +4,34 @@ import Link from 'next/link';
 
 const AdminDashboard = () => {
     return (
-        <div>
-            <h1>Welcome to the Admin Dashboard</h1>
-            <Link href="/event-manager">Manage Events</Link>
-            <br></br>
-            <Link href="/ticket-manager">Manage Tickets</Link>
-            <Link href="/qr-scanner">QR Scan</Link>
-        </div>
+        <section className="bg-gray-50 dark:bg-gray-900 min-h-screen">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-2xl font-bold leading-tight tracking-tight text-gray-900 md:text-3xl dark:text-white text-center">
+                            Welcome to the Admin Dashboard
+                        </h1>
+                        <div className="space-y-4 md:space-y-6">
+                            <Link href="/event-manager">
+                                <div className="block w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                    Manage Events
+                                </div>
+                            </Link>
+                            <Link href="/ticket-manager">
+                                <div className="block w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                    Manage Tickets
+                                </div>
+                            </Link>
+                            <Link href="/attendees-download">
+                                <div className="block w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                                    Download Attendee list
+                                </div>
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 };
 
