85cb39bc55ca0baa9cbfe91be080eaf956253237
Artilleryxd <karandwari@gmail.com>
2024-09-29 11:40:29 +0530

    Fixed success carousel

--- components/common/SuccessCarousel.js
+++ components/common/SuccessCarousel.js
@@ -1,40 +1,45 @@
-import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // import carousel styles
+import React, { useRef, useEffect } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import Item from './Item';
 import { SuccessCarouselData } from '@/assets/data/dummydata';
 
 export function SuccessCarousel(props) {
+  const sliderRef = useRef(null);
+
+  const settings = {
+    infinite: true,
+    speed: 6000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false, // Initially set autoplay to false
+    autoplaySpeed: 0,
+    pauseOnHover: true,
+    arrows: false,
+  };
+
+  useEffect(() => {
+    // Set a 5-second delay to start autoplay
+    const timer = setTimeout(() => {
+      if (sliderRef.current) {
+        sliderRef.current.slickPlay(); // Start autoplay after 5 seconds
+      }
+    }, 5000); // 5000 milliseconds = 5 seconds
+
+    // Clear timeout if the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <div 
-      style={{ 
-        display: 'flex', 
-        justifyContent: 'center', 
-        alignItems: 'center', 
-        padding: '20px 0', 
-        marginBottom: '40px',
-        maxWidth: '1000px', // Limit the max width for larger screens
-        margin: '0 auto', // Centering the carousel container
-        height: '500px', // Set a fixed height for the carousel
-      }}
-    >
-      <Carousel 
-        showArrows={props.showArrows} 
-        infiniteLoop
-        autoPlay
-        showThumbs={false}
-        showStatus={false}
-        emulateTouch
-        swipeable
-        interval={5000}
-        style={{ width: '100%', height: '100%' }} // Responsive width and height for carousel
-      >
+    <div>
+      <Slider ref={sliderRef} {...settings}>
         {SuccessCarouselData.map((item, index) => (
           <div key={index}>
             <Item src={item.src} title={item.title} description={item.description} />
           </div>
         ))}
-      </Carousel>
+      </Slider>
     </div>
   );
-};
+}
