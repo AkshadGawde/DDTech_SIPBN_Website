df767ad20049ac4bb8497775a07fa7fedc5ad4ef
Akshad Gawde <gawdeakshad@gmail.com>
2024-10-08 14:31:36 +0530

    Rearranged tickets

--- components/HeroPage/TicketPage.js
+++ components/HeroPage/TicketPage.js
@@ -14,38 +14,31 @@ function TicketPage() {
       </div>
 
       <div className="ticketContent">
+        {/* First Ticket (959) */}
         <div className="ticketCard">
-          <h3>#CapTech2024 SUMMIT PASS</h3>
+          <h3>#CapTech2024 + Post Networking</h3>
           <div
             className="customHr"
-            style={{ background: "linear-gradient(135deg, #b9fbc0, #00d2ff)" }}
+            style={{ background: "linear-gradient(135deg, #dcdcdc, #f5f5f5)" }}
           ></div>
-          {/* <h3>
-            Early Bird Price: <br /> (Save $300)
-          </h3> */}
-          <br />
-          <br />
-          <br />
-          <br />
-          <h2>$2,250</h2>
-          {/* <h4>Regular Price: $1,499</h4> */}
-          <br />
+          <h3>
+            Early Bird Price: <br /> (Save $240)
+          </h3>
+          <h2>$959</h2>
+          <h4>Regular Price: $1,199</h4>
           <CardDropDown
             content={{
-              l1: "NSW Parliament Drinks",
-              l2: "#CapTech2024 All Day Event + Post Event Drinks",
-              l3: "VIP seating & check-in",
-              l4: "Private Dinner",
-              l5: "Two Days of Dedicated Business Matching Sessions at Shangri-La Sydney",
-              l6: "Master class by world leader Michael Masterson on - How to go from pitching to catching for investors using your hidden assets",
+              l1: "#CapTech2024 Summit on Tuesday 26 November 2024 at ICC Sydney",
+              l2: "#CapTech2024 Summit on Tuesday 26 November 2024 post networking drinks",
             }}
-            maxHeight="660px"
+            maxHeight="300px"
           />
           <Link href={`/ticket-purchase?eventId=iaEHed2PE5Te0BmBwku3`}>
             <button className="ticketButton">Buy Now</button>
           </Link>
         </div>
 
+        {/* Second Ticket (1199) */}
         <div className="ticketCard">
           <h3>#CapTech2024 Summit + Welcome Drinks</h3>
           <div
@@ -70,29 +63,35 @@ function TicketPage() {
           </Link>
         </div>
 
+        {/* Third Ticket (2250) */}
         <div className="ticketCard">
-          <h3>#CapTech2024 + Post Networking</h3>
+          <h3>#CapTech2024 SUMMIT PASS</h3>
           <div
             className="customHr"
-            style={{ background: "linear-gradient(135deg, #dcdcdc, #f5f5f5)" }}
+            style={{ background: "linear-gradient(135deg, #b9fbc0, #00d2ff)" }}
           ></div>
-          <h3>
-            Early Bird Price: <br /> (Save $240)
-          </h3>
-          <h2>$959</h2>
-          <h4>Regular Price: $1,199</h4>
+          <br />
+          <br />
+          <br />
+          <br />
+          <h2>$2,250</h2>
           <CardDropDown
             content={{
-              l1: "#CapTech2024 Summit on Tuesday 26 November 2024 at ICC Sydney",
-              l2: "#CapTech2024 Summit on Tuesday 26 November 2024 post networking drinks",
+              l1: "NSW Parliament Drinks",
+              l2: "#CapTech2024 All Day Event + Post Event Drinks",
+              l3: "VIP seating & check-in",
+              l4: "Private Dinner",
+              l5: "Two Days of Dedicated Business Matching Sessions at Shangri-La Sydney",
+              l6: "Master class by world leader Michael Masterson on - How to go from pitching to catching for investors using your hidden assets",
             }}
-            maxHeight="300px"
+            maxHeight="660px"
           />
           <Link href={`/ticket-purchase?eventId=iaEHed2PE5Te0BmBwku3`}>
             <button className="ticketButton">Buy Now</button>
           </Link>
         </div>
 
+        {/* Fourth Ticket (9592) */}
         <div className="ticketCard">
           <h3>#CapTech2024 Corporate Table of 10</h3>
           <div
